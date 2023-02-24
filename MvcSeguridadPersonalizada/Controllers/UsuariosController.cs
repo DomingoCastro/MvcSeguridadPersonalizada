837c717c6131ecb6c4d6ae3f20de3a69edb2b20a
DomingoCastro <domingocastro1998@outlook.com>
2023-02-24 17:46:33 +0100

    Add project files.

---
+++ MvcSeguridadPersonalizada/Controllers/UsuariosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcSeguridadPersonalizada.Filters;
+
+namespace MvcSeguridadPersonalizada.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        [AuthorizeUser]
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcSeguridadPersonalizada/Filters/AuthorizeUserAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MvcSeguridadPersonalizada.Filters
+{
+    public class AuthorizeUserAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        //EN ESTE METODO ENTRARA CUANDO UN CONTROLADOR ESTE DECORADO (SE CREA CON CLICK DRCH NEW INTERFACE)
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //LOS USUARIOS SON ALMACENADOS DENTRO DE HttpContext Y DENTRO DE User
+            //UN USUARIO ESTA COMPUESTO POR UNA IDENTIDAD Y UN PRINCIPAL
+            //PODEMOS SABER EL NOMBRE DEL USUARIO O SI ESTA AUTENTICADO
+            var user = context.HttpContext.User;
+            if (user.Identity.IsAuthenticated == false) 
+            {
+                //SI EL USUARIO NO SE A VALIDADO PUES NO LE DEJAMOS ENTRAR
+                //Y LE LLEVAMOS A LOGIN
+                RouteValueDictionary rutalogin = new RouteValueDictionary(new
+                {
+                    controller = "Manager",
+                    action = "Login"
+                });
+                context.Result = new RedirectToRouteResult(rutalogin);
+            }
+        }
+    }
+}
